ea7b4d00d8961ce92e644704160fe1829b79baf4
VipulVerma77 <vipulverma4577@gmail.com>
2025-03-01 23:13:04 +0530

    Less work but fine

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname) // waise acchi baat nhii hai origanl name lena, twerk kr skte hai
+    }
+  })
+  
+ export  const upload = multer({
+     storage, 
+    })
